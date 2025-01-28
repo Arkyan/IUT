2b2d489094aab84c1daa99998263444fa50729b9
Jules Didry <didryjules@gmail.com>
2025-01-28 15:57:41 +0100

    Du c etc

---
+++ S2/R2.04 - Communication et fonctionnement bas niveau/TD/TD1/3.c
@@ -0,0 +1,11 @@
+//
+// Created by didry on 28/01/2025.
+//
+#include<stdio.h>
+int main(void) {
+  char chaine[]="Bonjour à tous\n";
+  int i;
+  for (i=0;i<5;i++)
+    printf("%s", chaine);
+  return 0;
+  }
---
+++ S2/R2.04 - Communication et fonctionnement bas niveau/TD/TD1/5.c
@@ -0,0 +1,21 @@
+//
+// Created by didry on 28/01/2025.
+//
+#include<stdio.h>
+void permut(int a, int b) {
+    int c;
+    c=a;
+    a=b;
+    b=c;
+}
+int main(void)
+{
+  int n1=5;
+    int n2=8;
+
+    printf("Avant permutation : n1=%d, n2=%d\n", n1, n2);
+    permut(n1, n2);
+
+    printf("Après permutation : n1=%d, n2=%d\n", n1, n2);
+    return 0;
+}
