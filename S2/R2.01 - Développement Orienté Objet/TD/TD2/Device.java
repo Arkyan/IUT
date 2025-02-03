f1c5e33c414f4eab8c4687ceefae335025004a71
Jules Didry <didryjules@gmail.com>
2025-02-03 21:48:12 +0100

    JAVA

--- S2/R2.01 - Développement Orienté Objet/TD/TD2/Device.java
+++ S2/R2.01 - Développement Orienté Objet/TD/TD2/Device.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Device {
     private final String name;
     private final Integer inventionYear;
@@ -5,6 +7,18 @@ public class Device {
     public Device(String name, Integer inventionYear) {
         this.name = name;
         this.inventionYear = inventionYear;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Device device = (Device) o;
+        return Objects.equals(name, device.name) && Objects.equals(inventionYear, device.inventionYear);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, inventionYear);
     }
 
     public String toString() {
---
+++ S2/R2.01 - Développement Orienté Objet/TP/TP1/HelloWorld.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = keyboard.nextLine();
+
+        int groupNumber;
+        do {
+            System.out.println("What is your group number?");
+            groupNumber = keyboard.nextInt();
+        } while (groupNumber < 1 || groupNumber > 3);
+
+        System.out.println("Hello, " + name + " from group " + groupNumber + "!");
+
+        keyboard.close();
+    }
+}
